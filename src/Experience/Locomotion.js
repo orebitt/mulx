c7ef3342f48a1de91883a801226a1277130f6c49
Andrew Moshkovich <thepythonhelper@gmail.com>
2023-05-09 11:02:59 -0700

    simplified line calculation

--- src/Experience/Locomotion.js
+++ src/Experience/Locomotion.js
@@ -16,14 +16,10 @@ let guidingController = null;
 // The guideline
 const lineSegments=10;
 const lineGeometry = new THREE.BufferGeometry();
-const lineGeometryVertices = new Float32Array((lineSegments +1) * 3);
+const lineGeometryVertices = new Float32Array((lineSegments + 1) * 3);
 lineGeometryVertices.fill(0);
-const lineGeometryColors = new Float32Array((lineSegments +1) * 3);
-lineGeometryColors.fill(0.5);
-lineGeometry.setAttribute('position', new THREE.BufferAttribute(lineGeometryVertices, 3));
-lineGeometry.setAttribute('color', new THREE.BufferAttribute(lineGeometryColors, 3));
-const lineMaterial = new THREE.LineBasicMaterial({ vertexColors: true, blending: THREE.AdditiveBlending });
-const guideline = new THREE.Line( lineGeometry, lineMaterial );
+const lineMaterial = new LineBasicMaterial({ color: 0x888888, blending: AdditiveBlending });
+const guideline = new Line( lineGeometry, lineMaterial );
 
 // The light at the end of the line
 const guidelight = new THREE.PointLight(0xffeeaa, 0, 2);
@@ -148,16 +144,13 @@ export default class Locomotion
             // Time for tele ball to hit ground
             const t = (-v.y  + Math.sqrt(v.y**2 - 2*p.y*g.y))/g.y;
     
-            const from = tempVec0;
-            const to = tempVec1;
-    
-            from.set(0,0,0);
+            const vertex = tempVec.set(0,0,0);    
             for (let i=1; i<=lineSegments; i++) {
     
                 // Current position of the virtual ball at time t, written to the variable 'to'
-                positionAtT(to,i*t/lineSegments,p,v,g);
-                guidingController.worldToLocal(to);
-                to.toArray(lineGeometryVertices,i*3);
+                positionAtT(vertex,i*t/lineSegments,p,v,g);
+                guidingController.worldToLocal(vertex);
+                vertex.toArray(lineGeometryVertices,i*3);
             }
             guideline.geometry.attributes.position.needsUpdate = true;
             
